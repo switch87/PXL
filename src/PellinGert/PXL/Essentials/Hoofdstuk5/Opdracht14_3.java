cf0f6e31783949e9faa1d2da8e4ea1bc5d63c128
Gert Pellin <gert@gepe-biljarts.be>
2015-03-18 11:28:48 +0100

    First Commit

---
+++ src/PellinGert/PXL/Essentials/Hoofdstuk5/Opdracht14_3.java
@@ -0,0 +1,9 @@
+package PellinGert.PXL.Essentials.Hoofdstuk5;
+
+public class Opdracht14_3 {
+	public static void main(String[] args)
+	{
+		for (int i=1;i<10000000;i*=11) System.out.println(i);
+	}
+
+}
