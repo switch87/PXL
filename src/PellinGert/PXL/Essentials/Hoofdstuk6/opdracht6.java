9f163bae850257ff5da9d3b539a2367475558b7a
Gert Pellin <gert@gepe-biljarts.be>
2015-03-24 15:32:49 +0100

    Hoofdstuk 6

---
+++ src/PellinGert/PXL/Essentials/Hoofdstuk6/opdracht6.java
@@ -0,0 +1,16 @@
+package PellinGert.PXL.Essentials.Hoofdstuk6;
+
+import java.util.GregorianCalendar;
+
+import javax.print.attribute.standard.DateTimeAtCreation;
+
+public class opdracht6 
+{
+	public static void main(String[] args) 
+	{
+		GregorianCalendar date = new GregorianCalendar();
+		GregorianCalendar geboorte = new GregorianCalendar(1987,2,5);
+		System.out.format("%1$td/%1$tm/%1$ty %1$tH:%1$tM:%1$tS\n",date);
+		System.out.format("%1$td/%1$tm/%1$ty %1$tH:%1$tM:%1$tS",geboorte);
+	}
+}
