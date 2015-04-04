f3152d9de268f5ea61eb1f1a9981159c7e8a6c0b
Gert <gert.pellin@student.pxl.be>
2015-04-04 23:17:52 +0200

    Hoofdstuk8

--- src/PellinGert/PXL/Essentials/Hoofdstuk8/Logo.java
+++ src/PellinGert/PXL/Essentials/Hoofdstuk8/Logo.java
@@ -1,38 +1,30 @@
 package PellinGert.PXL.Essentials.Hoofdstuk8;
-
+import java.util.*;
 /**
  * Created by gert on 4/04/2015.
  */
 public class Logo {
-    private Circle Circle;
-    private Rectangle Rectangle;
+    private ArrayList<Circle> Circles;
+    private ArrayList<Rectangle> Rectangles;
     private String Text;
 
-    public PellinGert.PXL.Essentials.Hoofdstuk8.Circle getCircle() {
-        return Circle;
+    public ArrayList<Circle> getCircle() {return Circles;}
+    public ArrayList<Rectangle> getRectangle() {return Rectangles;}
+    public String getText() {return Text;}
+    public void setCircle(ArrayList<Circle> circles) {Circles = circles;}
+    public void setRectangle(ArrayList<Rectangle> rectangles) {Rectangles = rectangles;}
+    public void setText(String text) {Text = text;}
+
+    public Logo(ArrayList<Circle> Circles, ArrayList<Rectangle> Rectangles, String Text){
+        setCircle(Circles);
+        setRectangle(Rectangles);
+        setText(Text);
     }
 
-    public PellinGert.PXL.Essentials.Hoofdstuk8.Rectangle getRectangle() {
-        return Rectangle;
+    public double getArea(){
+        double Area = 0;
+        for (Circle circle : Circles)Area+=circle.getArea();
+        for (Rectangle rectangle : Rectangles)Area+=rectangle.getArea();
+        return Area;
     }
-
-    public String getText() {
-        return Text;
-    }
-
-    public void setCircle(PellinGert.PXL.Essentials.Hoofdstuk8.Circle circle) {
-        Circle = circle;
-    }
-
-    public void setRectangle(PellinGert.PXL.Essentials.Hoofdstuk8.Rectangle rectangle) {
-        Rectangle = rectangle;
-    }
-
-    public void setText(String text) {
-        Text = text;
-    }
-
-    public Logo(Circle Circle, Rectangle Rectangle, String Text){setCircle(Circle);setRectangle(Rectangle);setText(Text);}
-
-    public double getArea(){return Circle.getArea()+Rectangle.getArea();}
 }
