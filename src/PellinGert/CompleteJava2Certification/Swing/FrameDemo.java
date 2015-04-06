2c3f66d661fb024dee82ea89309d4b26e2246f98
Gert <gert@gepe-biljarts.be>
2015-04-06 19:48:45 +0200

    Practicing some graphical components with a old book

---
+++ src/PellinGert/CompleteJava2Certification/Swing/FrameDemo.java
@@ -0,0 +1,14 @@
+package PellinGert.CompleteJava2Certification.Swing;
+
+import javax.swing.*;
+/**
+ * Created by gp on 6/04/15.
+ */
+
+public class FrameDemo {
+    public static void main(String[] args){
+        JFrame f = new JFrame("Frame Demo");
+        f.setSize(350,250);
+        f.setVisible(true);
+    }
+}
