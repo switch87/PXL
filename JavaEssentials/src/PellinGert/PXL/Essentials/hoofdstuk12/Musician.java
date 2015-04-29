24b01e8c5dd30c7c9c8449f6b0888e3d0d57126e
Gert Pellin <gert@gepe-biljarts.be>
2015-04-29 12:26:24 +0200

    Hoofdstuk 12: opdracht 1 en 2

---
+++ JavaEssentials/src/PellinGert/PXL/Essentials/hoofdstuk12/Musician.java
@@ -0,0 +1,18 @@
+package PellinGert.PXL.essentials.hoofdstuk12;
+
+/**
+ * Created by net04 on 29/04/2015.
+ */
+public class Musician {
+
+    public void play() {
+        Instrument instrument = new Instrument();
+        instrument.makeNoice();
+    }
+
+    class Instrument {
+        public void makeNoice() {
+
+        }
+    }
+}
